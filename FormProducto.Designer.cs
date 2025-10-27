15d28780856c6e668ce722acd2367b65eb2e8daa
lucas <lucasojeda2405@gmail.com>
2025-10-26 22:27:59 -0300

    Páginación en Productos

--- FormProducto.Designer.cs
+++ FormProducto.Designer.cs
@@ -34,6 +34,10 @@
             LVolver = new Label();
             BVolver = new Button();
             dataGridView1 = new DataGridView();
+            cmbTamanioPagina = new ComboBox();
+            labelPagina = new Label();
+            btnAnterior = new Button();
+            btnSiguiente = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
@@ -45,16 +49,18 @@
             panel1.Controls.Add(LProductos);
             panel1.Controls.Add(LVolver);
             panel1.Controls.Add(BVolver);
-            panel1.Location = new Point(12, 12);
+            panel1.Location = new Point(14, 16);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(906, 100);
+            panel1.Size = new Size(1035, 133);
             panel1.TabIndex = 0;
             // 
             // BAgregar
             // 
-            BAgregar.Location = new Point(755, 16);
+            BAgregar.Location = new Point(863, 21);
+            BAgregar.Margin = new Padding(3, 4, 3, 4);
             BAgregar.Name = "BAgregar";
-            BAgregar.Size = new Size(138, 57);
+            BAgregar.Size = new Size(158, 76);
             BAgregar.TabIndex = 3;
             BAgregar.Text = "Agregar Producto";
             BAgregar.UseVisualStyleBackColor = true;
@@ -64,27 +70,28 @@
             // 
             LProductos.AutoSize = true;
             LProductos.Font = new Font("Arial", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            LProductos.Location = new Point(392, 26);
+            LProductos.Location = new Point(448, 35);
             LProductos.Name = "LProductos";
-            LProductos.Size = new Size(173, 37);
+            LProductos.Size = new Size(215, 46);
             LProductos.TabIndex = 1;
             LProductos.Text = "Productos";
             // 
             // LVolver
             // 
             LVolver.AutoSize = true;
-            LVolver.Location = new Point(30, 76);
+            LVolver.Location = new Point(34, 101);
             LVolver.Name = "LVolver";
-            LVolver.Size = new Size(39, 15);
+            LVolver.Size = new Size(50, 20);
             LVolver.TabIndex = 1;
             LVolver.Text = "Volver";
             // 
             // BVolver
             // 
             BVolver.Image = Properties.Resources.volver;
-            BVolver.Location = new Point(12, 3);
+            BVolver.Location = new Point(14, 4);
+            BVolver.Margin = new Padding(3, 4, 3, 4);
             BVolver.Name = "BVolver";
-            BVolver.Size = new Size(80, 70);
+            BVolver.Size = new Size(91, 93);
             BVolver.TabIndex = 1;
             BVolver.UseVisualStyleBackColor = true;
             BVolver.Click += BVolver_Click;
@@ -92,28 +99,73 @@
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 202);
+            dataGridView1.Location = new Point(14, 269);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(906, 292);
+            dataGridView1.Size = new Size(1035, 389);
             dataGridView1.TabIndex = 2;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
-        
+            // 
+            // cmbTamanioPagina
+            // 
+            cmbTamanioPagina.FormattingEnabled = true;
+            cmbTamanioPagina.Location = new Point(20, 196);
+            cmbTamanioPagina.Name = "cmbTamanioPagina";
+            cmbTamanioPagina.Size = new Size(151, 28);
+            cmbTamanioPagina.TabIndex = 3;
+            cmbTamanioPagina.SelectedIndexChanged += cmbTamanioPagina_SelectedIndexChanged;
+            // 
+            // labelPagina
+            // 
+            labelPagina.AutoSize = true;
+            labelPagina.Location = new Point(19, 164);
+            labelPagina.Name = "labelPagina";
+            labelPagina.Size = new Size(60, 20);
+            labelPagina.TabIndex = 4;
+            labelPagina.Text = "Mostrar";
+            labelPagina.Click += labelPagina_Click;
+            // 
+            // btnAnterior
+            // 
+            btnAnterior.Location = new Point(304, 203);
+            btnAnterior.Name = "btnAnterior";
+            btnAnterior.Size = new Size(156, 29);
+            btnAnterior.TabIndex = 5;
+            btnAnterior.Text = "<< Anterior";
+            btnAnterior.UseVisualStyleBackColor = true;
+            btnAnterior.Click += btnAnterior_Click;
+            // 
+            // btnSiguiente
+            // 
+            btnSiguiente.Location = new Point(488, 203);
+            btnSiguiente.Name = "btnSiguiente";
+            btnSiguiente.Size = new Size(156, 29);
+            btnSiguiente.TabIndex = 6;
+            btnSiguiente.Text = ">> Siguiente";
+            btnSiguiente.UseVisualStyleBackColor = true;
+            btnSiguiente.Click += btnSiguiente_Click;
             // 
             // FormProducto
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Azure;
-            ClientSize = new Size(926, 562);
+            ClientSize = new Size(1058, 749);
+            Controls.Add(btnSiguiente);
+            Controls.Add(btnAnterior);
+            Controls.Add(labelPagina);
+            Controls.Add(cmbTamanioPagina);
             Controls.Add(dataGridView1);
             Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FormProducto";
             Text = "FormProducto";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -129,5 +181,9 @@
         private TextBox textBox2;
         private DataGridView dataGridView1;
         private Button BAgregar;
+        private ComboBox cmbTamanioPagina;
+        private Label labelPagina;
+        private Button btnAnterior;
+        private Button btnSiguiente;
     }
 }
--- FormProducto.cs
+++ FormProducto.cs
@@ -9,11 +9,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 namespace formLogin
 {
     public partial class FormProducto : Form
     {
+        int paginaActual = 1;
+        int tamanioPagina = 10;
+        int totalRegistros = 0;
+        int totalPaginas = 0;
 
         private Form _FormAnterior;
         //variable del formulario anterior
@@ -22,10 +27,15 @@ namespace formLogin
         string connectionString = "Server=localhost\\SQLEXPRESS;Database=BD_TALLER;Trusted_Connection=True;TrustServerCertificate=True;";
         int idSeleccionado = 0;     //Identifica el elemento de datagridview 
         private SqlConnection conn;
+        private object comboBox1;
 
         public FormProducto(Form formAnterior)
         {
             InitializeComponent();
+            // Configurar ComboBox
+            cmbTamanioPagina.Items.AddRange(new object[] { 5, 10, 20, 50 });
+            cmbTamanioPagina.SelectedIndex = 1; // valor inicial = 10
+            tamanioPagina = (int)cmbTamanioPagina.SelectedItem;
             CargarDatos();
             //      this.Load += FormProducto_Load;
 
@@ -36,26 +46,43 @@ namespace formLogin
         {
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                string query = "SELECT * FROM Productos";
+                conn.Open();
+
+                // Primero contar los registros totales
+                SqlCommand countCmd = new SqlCommand("SELECT COUNT(*) FROM Productos", conn);
+                totalRegistros = (int)countCmd.ExecuteScalar();
+                totalPaginas = (int)Math.Ceiling((double)totalRegistros / tamanioPagina);
+
+                int offset = (paginaActual - 1) * tamanioPagina;
+
+                string query = @"SELECT * FROM Productos
+                         ORDER BY id_producto
+                         OFFSET @offset ROWS FETCH NEXT @pageSize ROWS ONLY";
+
                 SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                da.SelectCommand.Parameters.AddWithValue("@offset", offset);
+                da.SelectCommand.Parameters.AddWithValue("@pageSize", tamanioPagina);
+
                 DataTable dt = new DataTable();
                 da.Fill(dt);
                 dataGridView1.DataSource = dt;
 
-
-                if (!dataGridView1.Columns.Contains("BEditar")) 
+                // Agregar el botón Editar solo si no existe
+                if (!dataGridView1.Columns.Contains("BEditar"))
                 {
                     DataGridViewButtonColumn btnColumna = new DataGridViewButtonColumn();
-                btnColumna.HeaderText = "Acción";
-                btnColumna.Name = "BEditar";
-                btnColumna.Text = "Editar";
-                btnColumna.UseColumnTextForButtonValue = true;
-
-                // Agregarla al DataGridView
-                dataGridView1.Columns.Add(btnColumna);
+                    btnColumna.HeaderText = "Acción";
+                    btnColumna.Name = "BEditar";
+                    btnColumna.Text = "Editar";
+                    btnColumna.UseColumnTextForButtonValue = true;
+                    dataGridView1.Columns.Add(btnColumna);
                 }
+
+                // Actualizar etiqueta o mostrar la página actual
+                labelPagina.Text = $"Página {paginaActual} de {totalPaginas}";
             }
         }
+
 
         //private void FormProducto_Load(object sender, EventArgs e)
         //     {
@@ -198,7 +225,36 @@ namespace formLogin
             }
         }
 
-       
+        private void cmbTamanioPagina_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tamanioPagina = (int)cmbTamanioPagina.SelectedItem;
+            paginaActual = 1;
+            CargarDatos();
+
+        }
+
+        private void labelPagina_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            if (paginaActual > 1)
+            {
+                paginaActual--;
+                CargarDatos();
+            }
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            if (paginaActual < totalPaginas)
+            {
+                paginaActual++;
+                CargarDatos();
+            }
+        }
     }
 }
 
